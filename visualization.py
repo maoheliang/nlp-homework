368ff7eb39772520f9c13c817c5247927e7b216b
Ge Yang <89033914+AboveParadise@users.noreply.github.com>
2025-06-04 14:51:04 +0800

    适配pdf_and_xlsx和word两个json中间结果的可视化代码

--- visualization.py
+++ visualization.py
@@ -2,51 +2,6 @@ import json
 import plotly.graph_objects as go
 from jinja2 import Template
 
-# 读取 JSON 数据
-# jsonfile = "mid_output.json"
-# jsonfile = "mid_output_from_raw_v2.json"
-jsonfile = "mid_output_pdf_and_xlsx.json"
-with open(jsonfile, "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-# 图表 1：参训人数柱状图
-departments = [item["name"] for item in data["col_2"]["data"]]
-join_values = [item["value"] for item in data["col_2"]["data"]]
-nojoin_values = [item["value"] for item in data["col_3"]["data"]]
-bar_chart = go.Figure(data=[
-    go.Bar(name='参训人数', x=departments, y=join_values, marker_color='rgb(52, 152, 219)'),
-    go.Bar(name='缺勤人数', x=departments, y=nojoin_values, marker_color='rgb(52, 152, 29)')
-])
-bar_chart.update_layout(
-    title={'text': '各科室参与培训情况', 'x': 0.5, 'xanchor': 'center'},
-    barmode='stack',
-    xaxis_title='科室', yaxis_title='人数'
-)
-
-# departments2 = [item["name"] for item in data["col_2"]["data"]]
-# join_values2 = [item["value"] for item in data["col_2"]["data"]]
-# bar_chart = go.Figure(data=[
-#     go.Bar(name='参训人数', x=departments2, y=join_values2, marker_color='rgb(52, 152, 219)')
-# ])
-# bar_chart.update_layout(
-#     title={'text': '各科室参与培训人数', 'x': 0.5, 'xanchor': 'center'},
-#     xaxis_title='科室', yaxis_title='人数'
-# )
-
-# 图表 2：问题分析饼图
-problem_labels = [item["name"] for item in data["col_3"]["data"]]
-problem_values = [item["value"] for item in data["col_3"]["data"]]
-problem = data["col_3"]["name"]
-pie_chart = go.Figure(data=[
-    go.Pie(labels=problem_labels, values=problem_values, hole=0.3)
-])
-pie_chart.update_layout(
-    title={'text': problem, 'x': 0.5, 'xanchor': 'center'}
-)
-
-# 转换为 HTML 片段
-bar_html = bar_chart.to_html(include_plotlyjs=False, full_html=False)
-pie_html = pie_chart.to_html(include_plotlyjs=False, full_html=False)
 
 # HTML 模板（含样式美化）
 html_template = """
@@ -65,6 +20,7 @@ html_template = """
     <style>
         body { font-family: "Segoe UI", sans-serif; margin: 40px; background: #f8f9fa; color: #2c3e50; }
         h1, h2 { color: #2c3e50; }
+
         .section { margin-bottom: 50px; background: #fff; padding: 30px; border-radius: 8px; box-shadow: 0 2px 8px rgba(0,0,0,0.05); }
 
         table { width: 60%; border-collapse: collapse; margin-top: 15px; margin: 0 auto;}
@@ -73,7 +29,7 @@ html_template = """
 
         p { line-height: 1.6; }
 
-        .chart-box { margin: 0 auto; max-width: 800px; }
+        .chart-box { max-width: 1400px; margin: 0 auto;  }
         .mermaid {
             margin: 0 auto; 
             max-width: 800px; 
@@ -100,8 +56,74 @@ html_template = """
     <!-- %%ORG_OVERVIEW_TABLE%% -->
         
     </div>
+    <!-- %%ORG_OVERVIEW_CHART%% -->
+    
 
     <div class="section">
+    <div class="quote-block">
+        <h2>{{ idx1 }}、总结&建议</h2>
+        <p>{{ evaluation }}</p>
+        <hr>
+        <p>1.{{ advice1 }}</p>
+        <p>2.{{ advice2 }}</p>
+        <p>3.{{ advice3 }}</p>
+    </div>
+    </div>
+    <div class="section">
+        <h2>{{ idx2 }}、流程图</h2>
+        <div class="mermaid">
+            {{ mermaid_code }}
+        </div>
+    </div>
+    <div class="section">
+        <h2>{{ idx3 }}、鱼骨图</h2>
+        
+    </div>
+    
+</body>
+</html>
+"""
+
+# 读取 JSON 数据
+# jsonfile = "mid_output.json"
+# jsonfile = "mid_output_from_raw_v2.json"
+# jsonfile = "mid_output_pdf_and_xlsx.json"
+jsonfile = "mid_output_word.json"
+with open(jsonfile, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+if "col_2" in data.keys() and "col_3" in data.keys():
+    num = 4
+# 图表 1：参训人数柱状图
+    departments = [item["name"] for item in data["col_2"]["data"]]
+    join_values = [item["value"] for item in data["col_2"]["data"]]
+    nojoin_values = [item["value"] for item in data["col_3"]["data"]]
+    bar_chart = go.Figure(data=[
+        go.Bar(name='参训人数', x=departments, y=join_values, marker_color='rgb(52, 152, 219)'),
+        go.Bar(name='缺勤人数', x=departments, y=nojoin_values, marker_color='rgb(52, 152, 29)')
+    ])
+    bar_chart.update_layout(
+        title={'text': '各科室参与培训情况', 'x': 0.5, 'xanchor': 'center'},
+        barmode='stack',
+        xaxis_title='科室', yaxis_title='人数', width=1400
+    )
+
+    # 图表 2：问题分析饼图
+    problem_labels = [item["name"] for item in data["col_3"]["data"]]
+    problem_values = [item["value"] for item in data["col_3"]["data"]]
+    problem = data["col_3"]["name"]
+    pie_chart = go.Figure(data=[
+        go.Pie(labels=problem_labels, values=problem_values, hole=0.3)
+    ])
+    pie_chart.update_layout(
+        title={'text': problem, 'x': 0.5, 'xanchor': 'center'}
+    )
+
+    # 转换为 HTML 片段
+    bar_html = bar_chart.to_html(include_plotlyjs=False, full_html=False)
+    pie_html = pie_chart.to_html(include_plotlyjs=False, full_html=False)
+    html_template = html_template.replace("<!-- %%ORG_OVERVIEW_CHART%% -->",
+    """<div class="section">
         <h2>二、{{caption2}}</h2>
         <div class="chart-box">
             {{ bar_chart | safe }}
@@ -113,30 +135,12 @@ html_template = """
         <div class="chart-box">
             {{ pie_chart | safe }}
         </div>
-    </div>
-
-    <div class="quote-block">
-        <h2>四、总结&建议</h2>
-        <p>{{ evaluation }}</p>
-        <hr>
-        <p>1.{{ advice1 }}</p>
-        <p>2.{{ advice2 }}</p>
-        <p>3.{{ advice3 }}</p>
-    </div>
-    <div class="section">
-        <h2>五、流程图</h2>
-        <div class="mermaid">
-            {{ mermaid_code }}
-        </div>
-    </div>
-    <div class="section">
-        <h2>六、鱼骨图</h2>
-        
-    </div>
+    </div>""")
     
-</body>
-</html>
-"""
+else:
+    bar_html, pie_html, problem1 = None, None, None
+    num = 2
+
 def generate_mermaid_graph(data):
     overview = {item['label']: item['value'] for item in data['orgOverview']['fields']}
     participation = {item['label']: item['value'] for item in data['trainingParticipation']['fields']}
@@ -154,19 +158,20 @@ def generate_mermaid_graph(data):
         last_step = node_id
 
     mermaid_text = f"""
-graph TD
-  A[启动专项培训\\n{get_safe(overview.get("培训主题"))}] --> B[组织部门: {get_safe(overview.get("组织部门"))}]
-  B --> C[培训形式: {get_safe(overview.get("培训形式"))}]
-  C --> D[培训内容实施]
-  D --> E[整体参训率: {get_safe(participation.get("整体参训率"))}]
-  E --> F[理论考试平均成绩: {get_safe(metrics.get("理论考试平均成绩"))}]
-  F --> G[操作合格率: {get_safe(metrics.get("操作评估合格率"))}]
-  G --> H{{是否达标}}
-  H -- 是 --> I[最终通过率: {get_safe(metrics.get("最终通过率"))}]
-  H -- 否 --> J[问题分析: {get_safe(participation.get("存在问题"))}]
-  I --> K[总结与持续改进]{step_nodes}
-"""
+            graph TD
+            A[启动专项培训\\n{get_safe(overview.get("培训主题"))}] --> B[组织部门: {get_safe(overview.get("组织部门"))}]
+            B --> C[培训形式: {get_safe(overview.get("培训形式"))}]
+            C --> D[培训内容实施]
+            D --> E[整体参训率: {get_safe(participation.get("整体参训率"))}]
+            E --> F[理论考试平均成绩: {get_safe(metrics.get("理论考试平均成绩"))}]
+            F --> G[操作合格率: {get_safe(metrics.get("操作评估合格率"))}]
+            G --> H{{是否达标}}
+            H -- 是 --> I[最终通过率: {get_safe(metrics.get("最终通过率"))}]
+            H -- 否 --> J[问题分析: {get_safe(participation.get("存在问题"))}]
+            I --> K[总结与持续改进]{step_nodes}
+            """
     return mermaid_text.strip()
+
 mermaid_code = generate_mermaid_graph(data)
 overview = data["orgOverview"]
 section_title = overview["section"]
@@ -182,20 +187,23 @@ for item in fields:
 html_table += "</table>"
 
 final_html = html_template.replace("<!-- %%ORG_OVERVIEW_TABLE%% -->", html_table)
+chinese_nums = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十']
 # 渲染 HTML
 template = Template(final_html)
 rendered_html = template.render(
     caption1=data['orgOverview']['section'],
     caption2=data['trainingParticipation']['section'],
-    
     bar_chart=bar_html,
     pie_chart=pie_html,
     evaluation=data["conclusionSummary"]['summary']['content'],
-    problem=data["col_3"]["name"],
+    problem=problem1,
     advice1=data['conclusionSummary']['rectificationFlow']['steps'][0]['value'],
     advice2=data['conclusionSummary']['rectificationFlow']['steps'][1]['value'],
     advice3=data['conclusionSummary']['rectificationFlow']['steps'][2]['value'],
-    mermaid_code=mermaid_code
+    mermaid_code=mermaid_code,
+    idx1=chinese_nums[num],
+    idx2=chinese_nums[num+1],
+    idx3=chinese_nums[num+2],
 )
 
 output = jsonfile[:-4] + "html"
